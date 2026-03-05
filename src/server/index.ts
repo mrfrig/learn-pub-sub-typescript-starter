0de1414d8b391c28bd183238a21c9a2c2b587bb2
Franklin Isalguez <frig.ds@gmail.com>
2026-03-05 09:14:51 -0400

    2-4 Add connection logic to server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,17 @@
+import amqp from "amqplib";
+
 async function main() {
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   console.log("Starting Peril server...");
+
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection was successful ");
+
+  process.on('SIGINT', () => {
+    console.log("Shutting down Peril server...");
+    conn.close();
+    process.exit(0);
+  });
 }
 
 main().catch((err) => {
