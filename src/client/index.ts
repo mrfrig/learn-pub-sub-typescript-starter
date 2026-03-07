c72ccb6410a52dd16fc786532169378ccb229a0d
Franklin Isalguez <frig.ds@gmail.com>
2026-03-07 10:20:27 -0400

    3-7 Add client REPL

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,10 +1,19 @@
 import amqp from "amqplib";
-import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import {
+  clientWelcome,
+  commandStatus,
+  getInput,
+  printClientHelp,
+  printQuit,
+} from "../internal/gamelogic/gamelogic.js";
 import {
   declareAndBind,
   SimpleQueueType,
 } from "../internal/pubsub/declareAndBind.js";
 import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState } from "../internal/gamelogic/gamestate.js";
+import { commandSpawn } from "../internal/gamelogic/spawn.js";
+import { commandMove } from "../internal/gamelogic/move.js";
 
 async function main() {
   const rabbitConnString = "amqp://guest:guest@localhost:5672/";
@@ -22,6 +31,45 @@ async function main() {
     PauseKey,
     SimpleQueueType.Transient,
   );
+
+  const state = new GameState(username);
+
+  while (true) {
+    const words = await getInput();
+    if (words.length === 0) {
+      continue;
+    }
+    const command = words[0];
+
+    switch (command) {
+      case "spawn":
+        commandSpawn(state, words);
+        break;
+
+      case "move":
+        commandMove(state, words);
+        break;
+
+      case "status":
+        await commandStatus(state);
+        break;
+
+      case "help":
+        printClientHelp();
+        break;
+
+      case "spam":
+        console.log("Spamming not allowed yet!");
+        break;
+
+      case "quit":
+        printQuit();
+        process.exit(0);
+
+      default:
+        console.log("I don't understand the command");
+    }
+  }
 }
 
 main().catch((err) => {
