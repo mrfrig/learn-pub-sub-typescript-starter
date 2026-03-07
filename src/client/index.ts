c48a62f98120732e559353b53645ef24100b6954
Franklin Isalguez <frig.ds@gmail.com>
2026-03-07 09:45:10 -0400

    3-5 Add logic to create and delete transient queues

--- src/client/index.ts
+++ src/client/index.ts
@@ -1,5 +1,27 @@
+import amqp from "amqplib";
+import { clientWelcome } from "../internal/gamelogic/gamelogic.js";
+import {
+  declareAndBind,
+  SimpleQueueType,
+} from "../internal/pubsub/declareAndBind.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+
 async function main() {
+  const rabbitConnString = "amqp://guest:guest@localhost:5672/";
   console.log("Starting Peril client...");
+
+  const conn = await amqp.connect(rabbitConnString);
+  console.log("Connection was successful ");
+
+  const username = await clientWelcome();
+
+  const [channel, queue] = await declareAndBind(
+    conn,
+    ExchangePerilDirect,
+    `pause.${username}`,
+    PauseKey,
+    SimpleQueueType.Transient,
+  );
 }
 
 main().catch((err) => {
